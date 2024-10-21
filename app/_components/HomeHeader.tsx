d27d29b6ea70425c78d2ee4209a5fbfc618bacae
David Ghinea <david.ghinea01@gmail.com>
2024-10-21 13:23:43 +0300

    added mobile header

--- app/_components/HomeHeader.tsx
+++ app/_components/HomeHeader.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Logo from "@/public/transparentlogo.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { Menu } from "lucide-react";
 
 export default function HomeHeader(): JSX.Element {
   return (
     <div className="absolute mb-8 mt-8 flex w-full justify-center overflow-hidden">
-      <header className="flex w-[814px] items-center justify-between">
+      <header className="flex w-[910px] items-center justify-between pl-[48px] pr-[48px]">
         <Image src={Logo} alt="Logo" height={56}></Image>
 
-        <nav className="flex items-center gap-8">
+        <nav className="hidden sm:flex sm:items-center sm:gap-8">
           <Link href="/">
             <h3>Home</h3>
           </Link>
@@ -17,8 +18,13 @@ export default function HomeHeader(): JSX.Element {
             <h3>About</h3>
           </Link>
         </nav>
+        <nav className="sm:hidden">
+          <Menu />
+        </nav>
       </header>
     </div>
   );
 }
-// needs resizing for mobile
+
+// Hamburger menu is unfunctional atm - TO DO
+// https://ui.shadcn.com/docs/components/sheet
