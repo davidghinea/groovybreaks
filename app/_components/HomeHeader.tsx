9d29261df915ba9d3bb743e5010076e83271a2a2
David Ghinea <david.ghinea01@gmail.com>
2024-10-22 23:53:07 +0300

    prototype of mobile navbar

--- app/_components/HomeHeader.tsx
+++ app/_components/HomeHeader.tsx
@@ -3,6 +3,13 @@ import Logo from "@/public/transparentlogo.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function HomeHeader(): JSX.Element {
   return (
@@ -18,9 +25,28 @@ export default function HomeHeader(): JSX.Element {
             <h3>About</h3>
           </Link>
         </nav>
-        <nav className="sm:hidden">
-          <Menu />
-        </nav>
+        <div className="sm:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <Menu />
+            </SheetTrigger>
+            <SheetContent side="left" className="w-[85dvw]">
+              <SheetHeader>
+                <nav className="mt-12 flex flex-col items-center gap-8">
+                  <SheetDescription className="text-left text-2xl hover:text-primary-foreground">
+                    <Link href="/">Home</Link>
+                  </SheetDescription>
+                  <SheetDescription className="text-left text-2xl hover:text-primary-foreground">
+                    <Link href="/about">About</Link>
+                  </SheetDescription>
+                  <SheetDescription className="text-left text-2xl hover:text-primary-foreground">
+                    <Link href="/dashboard">Dashboard</Link>
+                  </SheetDescription>
+                </nav>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
       </header>
     </div>
   );
@@ -28,3 +54,5 @@ export default function HomeHeader(): JSX.Element {
 
 // Hamburger menu is unfunctional atm - TO DO
 // https://ui.shadcn.com/docs/components/sheet
+
+//I have a Cambridge C2 English Proficiency certificate
