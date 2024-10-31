1332cac3f5a65596eeb4d48e7db87dded97cccb8
David Ghinea <david.ghinea01@gmail.com>
2024-10-31 13:10:15 +0200

    improved nav menu and removed desktop menu

--- app/_components/HomeHeader.tsx
+++ app/_components/HomeHeader.tsx
@@ -1,31 +1,47 @@
+"use client";
+
 import React from "react";
 import Logo from "@/public/transparentlogo.svg";
 import Image from "next/image";
 import Link from "next/link";
-import { Menu } from "lucide-react";
+import { Menu, User2 } from "lucide-react";
+import { usePathname } from "next/navigation";
+
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
+  SheetClose,
   SheetHeader,
   SheetTrigger,
+  SheetFooter,
+  SheetTitle,
 } from "@/components/ui/sheet";
 
+import { Separator } from "@/components/ui/separator";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function HomeHeader(): JSX.Element {
+  const pathname = usePathname();
+  const navLinks = [
+    { href: "/", label: "Home" },
+    { href: "/dashboard", label: "Dashboard" },
+    { href: "/about", label: "About" },
+  ];
+
   return (
     <div className="absolute mb-8 mt-8 flex w-full justify-center overflow-hidden">
       <header className="flex w-[910px] items-center justify-between pl-[48px] pr-[48px]">
-        <Image src={Logo} alt="Logo" height={56}></Image>
+        <Image src={Logo} alt="Logo" height={56} />
 
-        <nav className="hidden sm:flex sm:items-center sm:gap-8">
-          <Link href="/">
-            <h3>Home</h3>
-          </Link>
-          <Link href="/about">
-            <h3>About</h3>
-          </Link>
-        </nav>
-        <div className="sm:hidden">
+        <div>
           <Sheet>
             <SheetTrigger>
               <Menu />
@@ -33,23 +49,43 @@ export default function HomeHeader(): JSX.Element {
             <SheetContent side="left" className="w-[85dvw]">
               <SheetHeader>
                 <nav className="mt-12 flex flex-col items-center gap-8">
-                  <SheetDescription className="text-left text-2xl hover:text-primary-foreground">
-                    <Link href="/">Home</Link>
-                  </SheetDescription>
-                  <SheetDescription className="text-left text-2xl hover:text-primary-foreground">
-                    <Link href="/about">About</Link>
-                  </SheetDescription>
-                  <SheetDescription className="text-left text-2xl hover:text-primary-foreground">
-                    <Link href="/dashboard">Dashboard</Link>
-                  </SheetDescription>
+                  {navLinks.map(({ href, label }) => (
+                    <SheetClose key={href}>
+                      <Link
+                        href={href}
+                        className={`text-left text-2xl ${
+                          pathname === href ? "" : "text-muted-foreground"
+                        } hover:text-primary-foreground`}
+                      >
+                        {label}
+                      </Link>
+                    </SheetClose>
+                  ))}
                 </nav>
               </SheetHeader>
+              <div className="absolute bottom-0 left-0 m-[24px] h-[100px] w-[calc(100%-48px)]">
+                <Link href="/signin">
+                  <Card className="flex h-[100%] w-[100%] items-center pl-8">
+                    <div className="flex h-[56px] w-[56px] items-center justify-center rounded-full outline outline-1 outline-muted">
+                      <User2 className="h-[28px] w-[28px]" />
+                    </div>
+                    <Separator
+                      orientation="vertical"
+                      className="ml-6 mr-6 h-7"
+                    />
+
+                    <CardHeader className="pl-0">
+                      <CardTitle>Profile</CardTitle>
+                      <CardDescription>Log in to view profile.</CardDescription>
+                    </CardHeader>
+                  </Card>
+                </Link>
+              </div>
             </SheetContent>
           </Sheet>
         </div>
+        {/* Navigation menu for mobile */}
       </header>
     </div>
   );
 }
-
-// https://ui.shadcn.com/docs/components/sheet
